5de0a06b45716d54d9a9141ba55db2cc7e5e467c
sungwon-097 <csw2757@gmail.com>
2024-10-06 02:50:02 +0900

    Initial commit

---
+++ moa-server/src/main/java/com/security/moaserver/MoaServerApplication.java
@@ -0,0 +1,13 @@
+package com.security.moaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoaServerApplication.class, args);
+    }
+
+}
---
+++ moa-server/src/test/java/com/security/moaserver/MoaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.moaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
